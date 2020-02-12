1c49e77890b7ca6fee7ad5ae650b342b1016105d
Obafemi Fadairo <obafad94@gmail.com>
2020-02-11 23:22:59 -0500

    Hide auth_token, add lyric page parsing

--- rapprodigy.py
+++ rapprodigy.py
@@ -1,7 +1,26 @@
+import config
 import requests
-# import pdb
+import bs4 as bs
+import argparse
 
-r = requests.get('https://genius.com')
-print(r)
-print(r.text)
-# pdb.set_trace()
+# import pdb
+# import urllib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--song", help="This is the 'song' variable")
+
+args = parser.parse_args()
+query = args.song
+
+url = 'https://api.genius.com/search?access_token=' + config.auth_token + "&q=" + query
+url = url.encode("utf-8")
+
+response = requests.get(url)
+title = response.json()["response"]["hits"][0]["result"]["title"]
+url = response.json()["response"]["hits"][0]["result"]["url"]
+id = response.json()["response"]["hits"][0]["result"]["id"]
+
+url = response.json()["response"]["hits"][0]["result"]["url"]
+response = requests.get(url)
+soup = bs.BeautifulSoup(response.text)
+print(soup.find(class_='lyrics').get_text())
